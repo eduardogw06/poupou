434a0f16034c4fc75dd5166a9db9ff605b04711e
Eduardo Wolf <eduardogw06@gmail.com>
2022-09-14 15:10:27 -0300

    feat: auth route

---
+++ src/modules/accounts/useCases/authenticateUser/authenticateUserController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { AuthenticateUserUseCase } from "./authenticateUserUseCase";
+
+class AuthenticateUserController {
+    async handle(request: Request, response: Response): Promise<Response> {
+        const { password, email } = request.body;
+        console.log(request.body);
+        const authenticateUserUseCase = container.resolve(
+            AuthenticateUserUseCase
+        );
+
+        const token = await authenticateUserUseCase.execute({
+            email,
+            password,
+        });
+
+        return response.json(token);
+    }
+}
+
+export { AuthenticateUserController };
---
+++ src/modules/accounts/useCases/authenticateUser/authenticateUserUseCase.ts
@@ -0,0 +1,59 @@
+import { compare } from "bcrypt";
+import { inject, injectable, singleton } from "tsyringe";
+import { AppError } from "../../../../errors/AppError";
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { sign } from "jsonwebtoken";
+
+interface IRequest {
+    email: string;
+    password: string;
+}
+
+interface IResponse {
+    user: {
+        name: string;
+        email: string;
+    };
+    token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+    constructor(
+        @inject("UsersRepository")
+        private usersRepository: IUsersRepository
+    ) { }
+
+    async execute({ email, password }: IRequest): Promise<IResponse> {
+        const { SECRET_TOKEN } = process.env;
+
+        const user = await this.usersRepository.findByEmail(email);
+
+        if (!user) {
+            throw new AppError("E-mail or password incorrect!");
+        }
+
+        const passwordMatch = await compare(password, user.password);
+
+        if (!passwordMatch) {
+            throw new AppError("E-mail or password incorrect!");
+        }
+
+        const token = sign({}, SECRET_TOKEN, {
+            subject: user.uuid,
+            expiresIn: "1d",
+        });
+
+        const tokenReturn: IResponse = {
+            token,
+            user: {
+                name: user.name,
+                email: user.email
+            }
+        };
+
+        return tokenReturn;
+    }
+}
+
+export { AuthenticateUserUseCase };
