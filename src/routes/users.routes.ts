11bb638be5420cecc815701668f4db09e60b4d67
Eduardo Wolf <eduardogw06@gmail.com>
2022-10-03 11:22:29 -0300

    feat: update user routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,10 +1,21 @@
 import { Router } from "express";
+import multer from "multer";
+import uploadConfig from "../config/upload";
+import { ensureAuthenticated } from "../middleware/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
+import { UpdateUserController } from "../modules/accounts/useCases/updateUser/UpdateUserController";
+import { UpdateUserPhotoController } from "../modules/accounts/useCases/updateUserPhoto/updateUserPhotoController";
 
 const usersRoutes = Router();
 
+const uploadPhoto = multer(uploadConfig.upload("./tmp/photo"));
+
 const createUserController = new CreateUserController();
+const updateUserController = new UpdateUserController();
+const updateUserPhotoController = new UpdateUserPhotoController();
 
 usersRoutes.post("/", createUserController.handle);
+usersRoutes.put("/", ensureAuthenticated, updateUserController.handle);
+usersRoutes.patch("/update-photo", uploadPhoto.single("photo"), ensureAuthenticated, updateUserPhotoController.handle);
 
 export { usersRoutes };
