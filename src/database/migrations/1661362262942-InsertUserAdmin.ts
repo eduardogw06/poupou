a661c0232be3cb176039afd1a0b895346ef708bb
Eduardo Wolf <eduardogw06@gmail.com>
2022-10-03 16:16:24 -0300

    fix: add env params to create admin user

--- src/database/migrations/1661362262942-InsertUserAdmin.ts
+++ src/database/migrations/1661362262942-InsertUserAdmin.ts
@@ -1,21 +1,25 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { hash } from "bcrypt";
+import { MigrationInterface, QueryRunner } from "typeorm";
 import { User } from "../../modules/accounts/entities/User";
 
 export class InsertUserAdmin1661362262942 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
+        const { ADMIN_EMAIL, ADMIN_PASSWORD } = process.env;
+        const passwordHash = await hash(ADMIN_PASSWORD, 8);
+
         await queryRunner
-        .manager.save(
-            queryRunner.manager.create<User>(User, {
-                name: 'Admin',
-                email: 'eduardogw06@gmail.com',
-                password: 'admin123',
-                photo: null,
-                google_id: null,
-                is_admin: true,
-                dark_theme: true
-            })
-        )
+            .manager.save(
+                queryRunner.manager.create<User>(User, {
+                    name: 'Admin',
+                    email: ADMIN_EMAIL,
+                    password: passwordHash,
+                    photo: null,
+                    google_id: null,
+                    is_admin: true,
+                    dark_theme: true
+                })
+            )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
