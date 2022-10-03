ab1a1a95034913890495d658dcaad6136a2b8778
Eduardo Wolf <eduardogw06@gmail.com>
2022-10-03 16:20:08 -0300

    fix: use case var name

--- src/modules/accounts/useCases/updateUserPhoto/updateUserPhotoController.ts
+++ src/modules/accounts/useCases/updateUserPhoto/updateUserPhotoController.ts
@@ -6,10 +6,10 @@ class UpdateUserPhotoController {
     async handle(request: Request, response: Response): Promise<Response> {
         const { uuid } = request.user;
         const photo = request.file.filename;
-        
-        const updateUserAvatarUseCase = container.resolve(UpdateUserPhotoUseCase);
 
-        await updateUserAvatarUseCase.execute({ user_id: uuid , photo});
+        const updateUsePhotoUseCase = container.resolve(UpdateUserPhotoUseCase);
+
+        await updateUsePhotoUseCase.execute({ user_id: uuid, photo });
 
         return response.status(204).send();
     }
