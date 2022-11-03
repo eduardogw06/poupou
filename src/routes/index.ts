a3b277bb71f0f19a7ab4db03d71f207fc01a3b94
Eduardo Wolf <eduardogw06@gmail.com>
2022-11-03 01:28:38 -0300

    feat: transaction routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { authenticateRoutes } from "./authenticate.routes";
 import { categoriesRoutes } from "./categories.routes";
 import { targetsRoutes } from "./target.routes";
+import { transactionsRoutes } from "./transaction.routes";
 import { usersRoutes } from "./users.routes";
 
 const router = Router();
@@ -10,6 +11,7 @@ router.use("/users", usersRoutes);
 router.use(authenticateRoutes);
 router.use("/category", categoriesRoutes);
 router.use("/target", targetsRoutes);
+router.use("/transaction", transactionsRoutes)
 
 
 export { router };
