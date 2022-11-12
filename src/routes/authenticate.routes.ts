908ca47d069d229d14abc6d4e4c0448772e97e51
Eduardo Wolf <eduardogw06@gmail.com>
2022-11-11 22:06:50 -0300

    refact: login route

--- src/routes/authenticate.routes.ts
+++ src/routes/authenticate.routes.ts
@@ -5,6 +5,6 @@ const authenticateRoutes = Router();
 
 const authenticateUserController = new AuthenticateUserController();
 
-authenticateRoutes.post("/sessions", authenticateUserController.handle);
+authenticateRoutes.post("/login", authenticateUserController.handle);
 
 export { authenticateRoutes };
