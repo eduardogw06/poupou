2ddb41b01f84dee88f65d4afc0cc62a0f250ea95
Eduardo Wolf <eduardogw06@gmail.com>
2023-05-06 11:37:20 -0300

    fix: menus url

--- src/database/migrations/1672945670472-InsertMenus.ts
+++ src/database/migrations/1672945670472-InsertMenus.ts
@@ -10,56 +10,56 @@ export class InsertMenus1672945670472 implements MigrationInterface {
                     [
                         {
                             name: 'Meus objetivos',
-                            url: 'meus-objetivos',
+                            url: '/meus-objetivos',
                             icon: 'piggy-bank',
                             admin_only: false,
                             active: true
                         },
                         {
                             name: 'Meus aportes',
-                            url: 'meus-aportes',
+                            url: '/meus-aportes',
                             icon: 'dollar',
                             admin_only: false,
                             active: true
                         },
                         {
                             name: 'Aportes automáticos',
-                            url: 'aportes-automaticos',
+                            url: '/aportes-automaticos',
                             icon: 'robot',
                             admin_only: false,
                             active: true
                         },
                         {
                             name: 'Perfil',
-                            url: 'perfil',
+                            url: '/perfil',
                             icon: 'user',
                             admin_only: false,
                             active: true
                         },
                         {
                             name: 'Alterar Senha',
-                            url: 'alterar-senha',
+                            url: '/alterar-senha',
                             icon: 'key',
                             admin_only: false,
                             active: true
                         },
                         {
                             name: 'E-mails',
-                            url: 'emails',
+                            url: '/emails',
                             icon: 'envelope',
                             admin_only: true,
                             active: true
                         },
                         {
                             name: 'Notificações',
-                            url: 'notificacoes',
+                            url: '/notificacoes',
                             icon: 'bell',
                             admin_only: true,
                             active: true
                         },
                         {
                             name: 'Categorias',
-                            url: 'categorias',
+                            url: '/categorias',
                             icon: 'list',
                             admin_only: true,
                             active: true
