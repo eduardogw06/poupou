aa791a447be74440f6ccd188c2ef3469d1bef68d
Eduardo Wolf <eduardogw06@gmail.com>
2023-05-06 11:52:09 -0300

    fix: delete category

--- src/modules/target/useCases/category/deleteCategory/DeleteCategoryController.ts
+++ src/modules/target/useCases/category/deleteCategory/DeleteCategoryController.ts
@@ -1,12 +1,13 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 import { DeleteCategoryUseCase } from "./DeleteCategoryUseCase";
+import { getUserIdFromAuthHeader } from "../../../../../utils/getUserIdFromAuthHeader";
 
 class DeleteCategoryController {
     async handle(request: Request, response: Response): Promise<Response> {
-        const { category_id } = request.body;
+        const { category_id } = request.query;
         const deleteCategoryUseCase = container.resolve(DeleteCategoryUseCase);
-        await deleteCategoryUseCase.execute(category_id);
+        await deleteCategoryUseCase.execute(category_id as string);
 
         return response.status(204).send();
     }
