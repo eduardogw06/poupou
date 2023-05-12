09a4a380f06d5cd916ca2886251b3c9c9b9589f0
Eduardo Wolf <eduardogw06@gmail.com>
2023-05-11 23:48:17 -0300

    refact: get user id from auth header

--- src/modules/target/useCases/target/listTarget/ListTargetController.ts
+++ src/modules/target/useCases/target/listTarget/ListTargetController.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from "express";
 import { container } from "tsyringe";
 import { ListTargetUseCase } from "./ListTargetUseCase";
+import { getUserIdFromAuthHeader } from "../../../../../utils/getUserIdFromAuthHeader";
 
 class ListTargetController {
     async handle(request: Request, response: Response): Promise<Response> {
-        const { user_id, target_id } = request.query;
+        const { target_id } = request.query;
+        const user_id = getUserIdFromAuthHeader(request.headers.authorization);
+
         const listTargetUseCase = container.resolve(ListTargetUseCase);
         const targets = await listTargetUseCase.execute({
             user_id: user_id as string,
