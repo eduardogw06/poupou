4c7b7c38af55b99dd6eddbdc81e0cfbfc0dd24ef
Eduardo Wolf <eduardogw06@gmail.com>
2023-05-14 01:25:24 -0300

    feat: update transaction dto

--- src/modules/transaction/dtos/IUpdateTransactionsDTO.ts
+++ src/modules/transaction/dtos/IUpdateTransactionsDTO.ts
@@ -1,5 +1,10 @@
 interface IUpdateTransactionsDTO {
-
+    transaction_id: string;
+    user_id: string;
+    target_id: string;
+    type: "Aporte" | "Retirada";
+    amount: number;
+    date: Date;
 }
 
 export { IUpdateTransactionsDTO }
