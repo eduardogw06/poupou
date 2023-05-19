c7ab5e367c1bee2a066d1133bf310f5ea74503a2
Eduardo Wolf <eduardogw06@gmail.com>
2023-05-19 00:06:11 -0300

    fix: list automatic investment dto

--- src/modules/transaction/dtos/IListAutomaticInvestmentDTO.ts
+++ src/modules/transaction/dtos/IListAutomaticInvestmentDTO.ts
@@ -1,6 +1,7 @@
 export interface IListAutomaticInvestmentDTO {
     automatic_investments_uuid: string;
     automatic_investments_target_id: string;
+    target_uuid: string;
     target_description: string;
     target_user_id: string;
     automatic_investments_amount: number;
